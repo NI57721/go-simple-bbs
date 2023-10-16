78b7a3756a3021405476c6299ca6acd1e740c52d
NI57721 <NI57721@users.noreply.github.com>
2023-10-16 23:43:43 +0900

    WIP

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func foo(s string) string {
@@ -9,5 +10,22 @@ func foo(s string) string {
 }
 
 func main() {
-	fmt.Println(foo("bar"))
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		fmt.Fprint(w, "Hello from hi1!\n")
+	}
+	h2 := func(w http.ResponseWriter, _ *http.Request) {
+          fmt.Fprint(w, "<!DOCTYPE html><html><link rel=\"stylesheet\" type=\"text/css\" href=\"/assets/style.css\"><title>Simple BBS</title><style><!-- .foo{background:yellow;} --></style>out of p<br><p>inside of p, and <span class=\"foo\" style=\"font-weight: bold;\">inside of span</span></p></html>\n")
+	}
+
+	css := func(w http.ResponseWriter, _ *http.Request) {
+          fmt.Fprint(w, ".foo{color:red;}\n")
+	}
+
+	http.HandleFunc("/", h1)
+	http.HandleFunc("/about", h2)
+	http.HandleFunc("/assets/style.css", css)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
